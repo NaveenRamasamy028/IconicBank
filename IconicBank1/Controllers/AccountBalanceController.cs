e4e0a4c18216156a9206a8756aecb55e8e4e374e
NaveenRamasamy028 <88303212+NaveenRamasamy028@users.noreply.github.com>
2022-03-03 14:06:12 +0530

    Add files via upload

---
+++ IconicBank1/Controllers/AccountBalanceController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IconicBank1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountBalanceController : ControllerBase
+    {
+        private readonly iconicbankingContext db;
+        public AccountBalanceController(iconicbankingContext _db)
+        {
+            db = _db;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetBalances()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ IconicBank1/Controllers/TransactionController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace IconicBank1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransactionController : ControllerBase
+    {
+        private readonly iconicbankingContext db;
+        public TransactionController(iconicbankingContext _db)
+        {
+            db = _db;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllTransactions()
+        {
+            return Ok(await db.Transactions.ToListAsync());
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddTransaction(Transaction e)
+        {
+            db.Transactions.Add(e);
+            await db.SaveChangesAsync();
+            return Ok();
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateTransaction(int id, Transaction e1)
+        {
+            Transaction e = db.Transactions.Find(id);
+            db.Entry(e).State = EntityState.Modified;
+            await db.SaveChangesAsync();
+            return Ok();
+        }
+        [HttpDelete]
+        public async Task<IActionResult> DeleteTransaction(int id)
+        {
+            Transaction e = db.Transactions.Find(id);
+            db.Transactions.Remove(e);
+            await db.SaveChangesAsync();
+            return Ok();
+        }
+        
+            
+}
+
+
+
+
+
+
+    }
+
